3128199f6e260df5e95f21d7b92f70020193bc37
yewyewXD <yewyew6933@gmail.com>
2021-03-20 09:36:14 +0800

    we now work for free

--- src/components/EligibleText.jsx
+++ src/components/EligibleText.jsx
@@ -3,35 +3,30 @@ import React from "react";
 const EligibleText = ({ employer, company, mission, strength, weakness }) => {
   return (
     <>
-      <p>
-        Dear {employer ? employer : "EMPLOYER_NAME"}, I'm a very passionate and
-        hardworking React frontend developer. People often see me as a friendly,
-        motivational, and optimistic person.
-      </p>
+      <p>Dear {employer ? employer : "EMPLOYER_NAME"},</p>
 
       <p>
         I love what {company ? company : "COMPANY_NAME"} is doing about{" "}
-        {mission ? mission : "COMPANY_MISSION"} and I'm excited to become a part
-        of the team.
+        {mission ? mission : "COMPANY_MISSION"}. I really want to work for{" "}
+        {company ? company : "COMPANY_NAME"} in France for free for 1 year, and
+        I'm excited to become a part of the team!
       </p>
 
       <p>
-        I'm experienced in {strength ? strength : "YOUR_STRENGTHS"}.
-        {weakness
-          ? ` I'm not familiar with ${weakness} but I'm so willing to learn them.`
-          : ""}
+        I'm a very passionate and hardworking React frontend developer. People
+        often see me as a friendly, motivational, and optimistic person.
+      </p>
+
+      <p>
+        I'm also a high-energy person, I would work at least 90 hours a week,
+        and I'm ready to bring the development of{" "}
+        {company ? company : "COMPANY_NAME"} to the next level!
       </p>
 
       <p>
         Please have a look at my portfolio:
         <br />
         https://yewkangwei.com/
-      </p>
-
-      <p>
-        I'm also a high-energy person, I would work 90 hours per week, and I'm
-        ready to bring the development of {company ? company : "COMPANY_NAME"}{" "}
-        to the next level!
       </p>
 
       <p>I look forward to hearing back from you. Thank you in advance.</p>
@@ -42,10 +37,7 @@ const EligibleText = ({ employer, company, mission, strength, weakness }) => {
         Yew
       </p>
 
-      <p>
-        **If you have decided that I'm not a good fit, please let me know how
-        I can improve, thank you!
-      </p>
+      <br />
     </>
   );
 };
