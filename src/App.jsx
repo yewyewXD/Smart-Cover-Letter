9987808838f933ad7738acf6876ce1d4611f7c57
yewyewXD <yewyew6933@hotmail.com>
2025-02-14 13:52:09 +0800

    fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import Eligible from './pages/Eligible.jsx'
 import Ineligible from './pages/Ineligible.jsx'
@@ -8,16 +8,18 @@ import SelectBar from './components/SelectBar.jsx'
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <SelectBar />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Navigate to="/eligible" />} />
-          <Route path="/eligible" element={Eligible} />
-          <Route path="/ineligible" element={Ineligible} />
-        </Routes>
-      </main>
+      <div>
+        <header>
+          <SelectBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Navigate to="/eligible" />} />
+            <Route path="/eligible" element={<Eligible />} />
+            <Route path="/ineligible" element={<Ineligible />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   )
 }
